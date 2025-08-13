08e5b2ae56a5d10f0709bdcc1e436eff43d7ef75
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-08-13 21:40:56 +0530

    update LICENSE and main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Stacked Trio Carousel'),
+      home: const MyHomePage(title: 'Smart Trio Card'),
     );
   }
 }
